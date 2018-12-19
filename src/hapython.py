b45b716d9e83e5eac15d840cb360dd6c079d3d49
avian <aviadnissel@gmail.com>
2018-12-19 20:44:28 +0200

    added quotation detection

--- src/hapython.py
+++ src/hapython.py
@@ -7,12 +7,48 @@ CONDITIONS = {"אחרת אם": "elif", "אחרת": "else", "אם": "if", "וגם
 PARSING = [PRINT, CONDITIONS]
 
 
-def parse_code(code):
-    # TODO Only repalce outside of ""
+def replace_part(part):
     for parsing in PARSING:
         for command in parsing.items():
-            code = code.replace(command[0], command[1])
-    return code
+            part = part.replace(command[0], command[1])
+    return part
+
+
+def find_quote_start(code, search_from=0):
+    single_quote = code.find("'", search_from)
+    double_quote = code.find('"', search_from)
+    if single_quote == -1:
+        return double_quote
+    if double_quote == -1:
+        return single_quote
+    return min(single_quote, double_quote)
+
+
+def parse_code(code):
+    parsed_code = ""
+    quote_type = None
+    quote_start = 0
+    quote_end = 0
+    prev_quote_end = 0
+
+    # TODO this is bad
+    quote_start = find_quote_start(code)
+    while quote_start > -1:
+        quote_type = code[quote_start]
+        quote_end = code.find(quote_type, quote_start + 1)
+        while quote_end != -1 and code[quote_end - 1] == "\\":
+            quote_end = code.find(quote_type, quote_end + 1)
+        if quote_end == -1:
+            quote_end = len(code) - 1
+        part = code[prev_quote_end:quote_start]
+        parsed_code += replace_part(part)
+        parsed_code += code[quote_start:quote_end]
+        prev_quote_end = quote_end
+        quote_start = find_quote_start(code, quote_end + 1)
+
+    part = code[quote_end:]
+    parsed_code += replace_part(part)
+    return parsed_code
 
 
 def execute_code(code):
